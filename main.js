742f0a5ead1339b2917666af79e8c247374b9bad
anasuba <163418434+anasuba@users.noreply.github.com>
2024-03-15 10:06:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem") .src=" https://oficinadeescrita.com.br/wp-content/uploads/2019/07/Para-ler-como-um-escritor-a-leitura-atenta-para-aprimorar-seus-textos.jpg"
+}
+function mudarimagemdentro( )
+{
+    document.getElementById("imagem") .src="https://cfvila.com.br/blog/wp-content/uploads/2019/10/clubes-leitura.jpg"
+}
